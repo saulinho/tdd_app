35e00188b0f8f5f21dbd7190370d6ffe8f44ee2e
saulinho <saulomorais@saulinho>
2025-05-19 15:34:53 -0300

    Cadastro de cliente parte 2

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ group :development, :test do
   gem "rspec-rails"
   gem "capybara"
   gem "faker"
+  gem "byebug"
 end
 
 group :development do
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,4 +5,21 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      redirect_to customers_path, notice: "Cliente cadastrado com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
+      puts "Retorno #{response.status}"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.expect(customer: [ :name, :email, :phone, :avatar, :smoker ])
+  end
 end
